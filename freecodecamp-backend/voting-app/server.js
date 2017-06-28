9b9f3324550c4570b4541ffceb98cf15eaaafd95
Eliot Glairon <ejg_dnd@yahoo.com>
2017-06-28 01:17:02 +0000

    Added some api calls, except voting doesn't seem to work.

--- freecodecamp-backend/voting-app/server.js
+++ freecodecamp-backend/voting-app/server.js
@@ -1,7 +1,9 @@
-
+//TODO: figure out why there seems to be a double execute sometime with twitch
+//passport
 
 //installed express, mongoose/mongodb, passport-twitch
 const express = require('express');
+const app = express();
 const server = "test-eliotn.c9users.io";
 const port = process.env.PORT || 3000;
 
@@ -10,13 +12,23 @@ const port = process.env.PORT || 3000;
 
 const mongourl = 'mongodb://localhost:27017/twitchvotes';
 var MongoClient = require('mongodb').MongoClient;
+
+//allow JSON request bodies to be readable
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 //users contains the following
 //access_token, userid
+//polls contains the following
+//userid, question, [{answer, votes}]
 var users;//get the collection
+var polls;//get the polls
 MongoClient.connect(mongourl, function(err, database) {
     if (err) throw err;
     users = database.collection('users');
-    users.drop();//temporary, drop between runs
+    //users.drop();//temporary, drop between runs
+    polls = database.collection('polls');
+    //polls.drop();
 });
 
 const fs = require('fs');
@@ -26,7 +38,7 @@ const fs = require('fs');
 //test out twitch passport
 const passport = require("passport");
 
-const app = express();
+
 app.use(passport.initialize());
 app.use(passport.session());
 var twitchStrategy = require("passport-twitch").Strategy;
@@ -58,6 +70,8 @@ fs.readFile("secret", "utf-8", function(err, str) {
             }
             else {
                 console.log("user already logged in!");
+                users.updateOne({"userid": profile.id}, {$set: {"access_token":accessToken}});
+                
                 return done(null, {"access_token":accessToken});
             }
         });
@@ -104,8 +118,70 @@ function afterPassport() {
     app.get('/', function (req, res) {
         res.send("Homepage");
     });
-    app.get('/api/testlogin', passport.authenticate("bearer", {session: false}), function (req, res) {
-        res.json({"Note":"You should only see this if you are authenticated."});
+    app.put('/api/poll/:pollid/:vote', function (req, res) {
+        console.log("Starting vote!");
+        polls.findOne({"userid": String(req.params.pollid)}, function(err, results) {
+            if (err) {
+                res.json({"err":err});
+                return;
+            }
+            console.log(results);
+            console.log(req.params.pollid);
+            if (!results || results.answers.length <= req.params.vote) {
+                res.status(404).send();
+                return;
+            }
+            polls.updateOne({userid: req.params.userid, answers:req.params.vote-1}, {$inc: "answers.$"});
+            res.json({"Note:": "vote counted"});
+        });
+    });
+    app.post('/api/poll',  passport.authenticate("bearer", {session: false}), function (req, res) {
+        console.log(req.body);
+        if (req.body.question && req.body.answers) {
+            polls.findOne({"userid": req.user.userid}, function(err, results) {
+            if (err) {
+                res.json({"err":err});
+                return;
+            }
+            //properly format answers
+            var answers = [];
+            for (var answer in req.body.answers) {
+                answers.push({"answer":answer, "votes":0});
+            }
+            
+            if (!results) {
+                console.log("poll needs to be created");
+                polls.insert({"userid": req.user.userid, "question": req.body.question, "answers":answers}, function (err) {
+                    if (err) res.json({"err":err});
+                    else res.json({"Note":"Poll added"});
+                });
+            }
+            else {
+                console.log("update poll with new information");
+                polls.updateOne({"userid": req.user.userid}, {$set: {"question":req.body.question, "answers":answers}});
+                res.json({"Note":"Poll updated"});
+            }
+        });
+        }
+        else {
+            res.json({"Note":"You posted an invalid poll"});
+        }
+    });
+    app.get('/api/debug/testlogin',  passport.authenticate("bearer", {session: false}), function (req, res) {
+        res.json({"Note":"Your userid is " + req.user.userid});
+    });
+    //dump the database output
+    app.get('/api/debug/users',  passport.authenticate("bearer", {session: false}), function (req, res) {
+        users.find().toArray(function(err, result) {
+            if (err) throw err;
+            res.json({'users':JSON.stringify(result)}); 
+        });
+    });
+    app.get('/api/debug/polls',  passport.authenticate("bearer", {session: false}), function (req, res) {
+        polls.find().toArray(function(err, result) {
+            if (err) throw err;
+            res.json({'polls':JSON.stringify(result)}); 
+        });
     });
     app.listen(port, function() {
         console.log("listening on port " + port);
